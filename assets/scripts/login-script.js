5218683e70d4d7287ee0c53ab9f1fa9d39e09603
Lexxsie <142602153+Lexxsie@users.noreply.github.com>
2025-08-11 22:44:52 +0700

    Update login-script.js
    Mengganti komentar dengan aturan JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,4 +1,33 @@
-
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
+const loginFormElement = document.querySelector('#loginForm');
+ 
+ 
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
+const inputEmailElement = document.querySelector('#inputEmail');
+ 
+ 
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
+const inputPasswordElement = document.querySelector('#inputPassword');
+ 
+ 
+/** 
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
+const expectedEmail = 'admin@dicoding.com';
+ 
+ 
+/** 
  * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
  * @constant {string}
  */
